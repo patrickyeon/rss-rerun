cd1f9887465e9f135a5428be6417c9850dcfbcbd
Patrick Yeon <patrickyeon@gmail.com>
2014-02-26 21:59:48 -0800

    re-org pages and sketch out an intro page

--- ruby/lib/app.rb
+++ ruby/lib/app.rb
@@ -4,17 +4,25 @@ require 'erb'
 require 'cgi'
 
 get '/' do
-    feed = Rerun.new(params[:url], DateTime.now - 9)
+    erb :home
+end
+
+get '/preview' do
+    if params[:url] == nil
+        redirect to('/'), 302
+    end
+
+    feed = Rerun.new(params[:url], params[:startDate], params[:schedule] || '0')
     feed.shift_entries
 
-    rss_url = 'http://localhost:4567/rerun.rss?url=' +  CGI::escape(params[:url])
+    rss_url = 'http://localhost:4567/rerun?url=' +  CGI::escape(params[:url])
     rss_url += '&startTime=' + (DateTime.now - 9).rfc822
     erb :preview, :locals => {:items => feed.preview_feed,
                               :feed_url => params[:url],
                               :rerun_url => rss_url}
 end
 
-get '/rerun.rss' do
+get '/rerun' do
     feed = Rerun.new(params[:url], DateTime.parse(params[:startTime]))
     feed.shift_entries
     feed.to_xml
