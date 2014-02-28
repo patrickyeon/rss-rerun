a4ecb9d4bac97e4dd107986616aad26b04710300
Patrick Yeon <patrickyeon@gmail.com>
2014-02-27 23:43:50 -0800

    Have schedule working, but not startdate

--- lib/app.rb
+++ lib/app.rb
@@ -12,18 +12,33 @@ get '/preview' do
         redirect to('/'), 302
     end
 
-    feed = Rerun.new(params[:url], params[:startDate], params[:schedule] || '0')
+    feed = Rerun.new(params[:url],
+                     DateTime.parse(params[:startDate]),
+                     sched_from(params))
     feed.shift_entries
 
     rss_url = 'http://localhost:4567/rerun?url=' +  CGI::escape(params[:url])
-    rss_url += '&startTime=' + (DateTime.now - 9).rfc822
+    rss_url += '&startDate=' + (DateTime.now - 9).rfc822
     erb :preview, :locals => {:items => feed.preview_feed,
                               :feed_url => params[:url],
                               :rerun_url => rss_url}
 end
 
 get '/rerun' do
-    feed = Rerun.new(params[:url], DateTime.parse(params[:startTime]))
+    feed = Rerun.new(params[:url],
+                     DateTime.parse(params[:startDate]),
+                     '0123')
+                     #sched_from(params))
     feed.shift_entries
     feed.to_xml
+end
+
+def sched_from(params)
+    retstr = ''
+    ['sun', 'mon', 'tue', 'wed', 'thu', 'fri', 'sat'].each_with_index do |day, i|
+        if params.has_key?(day)
+            retstr += String(i)
+        end
+    end
+    retstr
 end
