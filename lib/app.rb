aa5e8931bea6fc41a5192fc5faaa0226fd61c7d5
Patrick Yeon <patrickyeon@gmail.com>
2014-02-28 08:47:15 -0800

    now pass schedule and startDate around, it works but seems inelegant

--- lib/app.rb
+++ lib/app.rb
@@ -3,6 +3,8 @@ require 'sinatra'
 require 'erb'
 require 'cgi'
 
+Weekdays = ['sun', 'mon', 'tue', 'wed', 'thu', 'fri', 'sat']
+
 get '/' do
     erb :home
 end
@@ -12,13 +14,15 @@ get '/preview' do
         redirect to('/'), 302
     end
 
+    schedule = sched_from(params)
     feed = Rerun.new(params[:url],
                      DateTime.parse(params[:startDate]),
-                     sched_from(params))
+                     schedule)
     feed.shift_entries
 
     rss_url = 'http://localhost:4567/rerun?url=' +  CGI::escape(params[:url])
     rss_url += '&startDate=' + (DateTime.now - 9).rfc822
+    schedule.chars {|c| rss_url += '&' + Weekdays[c.to_i]}
     erb :preview, :locals => {:items => feed.preview_feed,
                               :feed_url => params[:url],
                               :rerun_url => rss_url}
@@ -27,15 +31,14 @@ end
 get '/rerun' do
     feed = Rerun.new(params[:url],
                      DateTime.parse(params[:startDate]),
-                     '0123')
-                     #sched_from(params))
+                     sched_from(params))
     feed.shift_entries
     feed.to_xml
 end
 
 def sched_from(params)
     retstr = ''
-    ['sun', 'mon', 'tue', 'wed', 'thu', 'fri', 'sat'].each_with_index do |day, i|
+    Weekdays.each_with_index do |day, i|
         if params.has_key?(day)
             retstr += String(i)
         end
