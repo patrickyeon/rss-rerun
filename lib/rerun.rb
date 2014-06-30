beebb54d2cdfd333d4369b8db2593a6f4e74757c
Patrick Yeon <patrickyeon@gmail.com>
2014-06-29 21:42:36 -0700

    removing cruft

--- lib/rerun.rb
+++ lib/rerun.rb
@@ -16,59 +16,6 @@ class Rerun
         end
     end
 
-    def shift_entries
-        # TODO make sure this only happens once. For now, it's just a private
-        #        method and we call it during initialization
-
-        if Chrono.now < @startTime
-
-            @feed.items.each {|e| e.remove}
-            return
-        end
-
-        # make sure we have our namespace here
-        unless @feed.feed.root.namespaces.key('xmlns:rerun')
-            # TODO find a real URL for the namespace to point to
-            @feed.feed.root.add_namespace_definition('rerun',
-                                                     'https://github.com/patrickyeon/rerun-rss')
-        end
-
-        repubDate = @startTime
-        count = 0
-        entries = @feed.items
-
-        # TODO this needs a lot more logic to work around what happens as we
-        #        catch up with the original feed.
-        while (repubDate < Chrono.now) and (count < entries.length) do
-            if @schedule.include?(repubDate.wday.to_s)
-                entry = entries.at(count)
-
-                if entry.at('pubDate') != nil
-                    odate = Nokogiri::XML::Node.new 'rerun:origDate', @feed.feed
-                    odate.content = entry.at('pubDate').to_str
-                    entry.add_child odate
-
-                    if entry.at('description') != nil
-                        # add a "originally published on" date to the description
-                        datestr = "\n<p> Originally published on %s</p>" % odate.content
-                        entry.at('description').content += datestr
-                    end
-                else
-                    entry.add_child Nokogiri::XML::Node.new('pubDate', @feed.feed)
-                end
-                entry.at('pubDate').content = repubDate.rfc822
-
-                count += 1
-            end
-            repubDate += 1
-        end
-
-        # clear out any entries that aren't to be replayed yet
-        entries[count .. -1].each do |e|
-            @feed.items.delete e
-        end
-    end
-
     def shifted
         if Chrono.now < @startTime
             return nil
@@ -140,6 +87,4 @@ class Rerun
         items[1..-1].each {|item| chan.children.after(item)}
         return ret.to_xml
     end
-
-    private :shift_entries
 end
